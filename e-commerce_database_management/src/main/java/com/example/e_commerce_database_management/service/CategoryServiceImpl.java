a82b6c2d2b49b3a5a321274417b810add987beb8
aslıhan yoldaş <aslihanyoldas24@gmail.com>
2025-06-14 14:40:02 +0300

    Categories added from the api to the database. The errors fixed about CategoryService while importing categories

--- e-commerce_database_management/src/main/java/com/example/e_commerce_database_management/service/CategoryServiceImpl.java
+++ e-commerce_database_management/src/main/java/com/example/e_commerce_database_management/service/CategoryServiceImpl.java
@@ -8,7 +8,7 @@ import com.example.e_commerce_database_management.apiClient.ProductApiClient;
 import com.example.e_commerce_database_management.constants.ConstantValues;
 import com.example.e_commerce_database_management.entity.Category;
 import com.example.e_commerce_database_management.repository.CategoryRepository;
-
+import org.apache.commons.text.WordUtils;
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
@@ -21,21 +21,25 @@ public class CategoryServiceImpl implements CategoryService {
     @Override
     public List<String> importCategories() {
         List<String> apiCategories = productApiClient.getCategories();
-        Category parent=null;
         for (String catName : apiCategories) {
             // Get the parent name (for example for women clothing get fashion )
             String parentName = ConstantValues.PARENT_CATEGORY_MAP.get(catName);
+            // Java requires any variables accessed inside a lambda to be final or effectively final.
+            // For that reason I use array
+            Category[] parent = new Category[1];
 
-            if (parentName != null) {
+            if (parentName != "") {
+                
                 // get the parent category from the database
                 // if parent not exist on database insert to the database
-                parent = categoryRepository.findByCategoryName(parentName)
-                        .orElse(categoryRepository.save(new Category(null, parentName, null)));
+                parent[0] = categoryRepository.findByCategoryName(WordUtils.capitalizeFully(parentName))
+                        .orElseGet(()->categoryRepository.save(new Category(null, parentName, null)));
 
             }
             // if category name from api does not exist insert to the table with its parent if it exists
-            categoryRepository.findByCategoryName(catName)
-                    .orElse(categoryRepository.save(new Category(null, catName, parent!= null?parent.getId():null)));
+            categoryRepository.findByCategoryName(WordUtils.capitalizeFully(catName))
+                    .orElseGet(()->categoryRepository.save(
+                        new Category(null, WordUtils.capitalizeFully(catName), parent!= null?parent[0].getId():null)));
 
         }
         return apiCategories;
